d21c15f382407dac4363aa7de2675ff10840eca2
hbangaruu <44521721+hbangaruu@users.noreply.github.com>
2022-03-30 17:42:01 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 
 provider "google" {
-	project = "revanth-gcp-2020"
+	project = "test-cloudbuild-8712"
 	region = "us-central1"
 	zone = "us-central1-a"
-	credentials = "./SA_key.json"
+	credentials = "./empty.json"
 }
